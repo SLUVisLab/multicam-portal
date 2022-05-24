33cbddfbacbfce1fbacdfe052e0aada14c8cf374
austin <ac6404@gmail.com>
2022-05-24 14:15:54 -0500

    add compund queries, no results, descriptive filename

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -15,17 +15,20 @@ function json_to_csv(data){
 
 }
 
-// Search Firestore DB.
-// express-validator accepts an array of functions
-// TODO: HANDLE EMPTY SEARCH RESULTS!!
-exports.image_search = [
-    //function1: 
-    //validate and sanitize user inputs
-    body('site', 'Site name required').trim().isLength({
-        min:1
-    }).escape(),
 
-    //function2:
+
+// Search Firestore DB.
+exports.image_search = [
+
+    //validate and sanitize user inputs
+    body('site', 'Site name required').trim().isLength({min:1}).escape(),
+
+    body('block', 'Block must be a number').trim().isInt().escape().optional({checkFalsy: true}),
+
+    body('beforeDate', 'Before date must be a valid date').trim().isDate().optional({checkFalsy: true}),
+
+    body('afterDate', 'After date must be a valid date').trim().isDate().optional({checkFalsy: true}),
+
     //process the request after validation and sanitation
     (req, res, next) => {
         //extract validation errors from a request
@@ -34,11 +37,14 @@ exports.image_search = [
         //extract data from the request
         var siteQuery = req.body.site;
         var blockQuery = req.body.block;
-        var dateAfter = req.body.dateAfter;
-        var dateBefore = req.body.dateBefore;
+        var afterDate = req.body.afterDate;
+        var beforeDate = req.body.beforeDate;
+
+        //use query to construct csv filename
+        let filename = `data_${siteQuery}_${blockQuery ? blockQuery : "xblock"}_${afterDate ? afterDate : "xafter"}_${beforeDate ? beforeDate : "xbefore"}`
         
         if (!errors.isEmpty()) {
-            //there are errors. Render the form afain with sanitized values/error messages
+            //there are errors. Render the form again with sanitized values/error messages
             res.render("search", {title: "Search",
         site: siteQuery, errors: errors.array()});
         return
@@ -47,10 +53,33 @@ exports.image_search = [
             //Pull data from firestore!
             console.log("Making request to firestore...")
             console.log(`site: ${req.body.site}`)
-            var results = db.collection('images').where('siteId', '==', req.body.site).get()
+            var query = db.collection('images').where('siteId', '==', siteQuery)
+            if(blockQuery) {
+                query = query.where('blockId', '==', blockQuery)
+            }
+
+            if(afterDate) {
+                let start = new Date(afterDate)
+                query = query.where('sessionStart', '>', start)
+            }
+
+            if(beforeDate) {
+                let end = new Date(beforeDate)
+                query = query.where('sessionStart', '<', end)
+            }
+
+            query.get()
                 .then(items => {
+                    //no results.
+                    if (!items.size || items.size < 1) {
+                        res.render("search", {title: "Search",
+                            site: siteQuery, errors: [{msg: "No Results Found"}]});
+                            return
+                    }
+                    
+                    
                     let dataArr = []
-                    let filename = "multi_cam_data"
+                    // convert dates to standard format and then convert to csv
                     items.forEach(documentSnapshot => {
                         let doc = documentSnapshot.data()
                         doc['creationDate'] = doc.creationDate.toDate()
